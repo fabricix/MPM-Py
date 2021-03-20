aa08a847da71d7763e9a1440f87477beab798e36
Fabricio Fernandez <fabricio.hmf@gmail.com>
2021-03-20 13:41:52 -0300

    mesh test file

---
+++ mesh-test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 13:39:50 2021
+
+Author
+------
+Fabricio Fernandez (<fabricio.hmf@gmail.com>)
+
+Purpose
+-------
+
+This examples tests the mesh creation function.
+
+"""
+
+# local modules
+from modules import mesh # for mesh generation
+from modules import material # for material definition
+
+# domain
+L = 2
+
+# elements
+nelem = 2
+
+# particles per element
+p_per_elem = 2
+
+# create an 1D mesh
+msh = mesh.mesh_1D(L,nelem)
+
+# define a linear material 
+elastic = material.linear_elastic(E=50,density=1)
+ 
+# put particles in elements
+msh.put_particles_in_mesh(ppelem=p_per_elem,material=elastic)
+
+#show mesh
+msh.print_mesh()
